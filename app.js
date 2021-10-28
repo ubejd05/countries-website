b135cd68acf9657deae9d6deead1e2703118f183
Ubejd <ubejd.haziri2005@gmail.com>
2021-10-28 20:03:05 +0200

    regions dropdown hide/show and placeholder

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const search = document.getElementById('search');
 const ctrSection = document.getElementById('countries');
 const modeBtn = document.getElementById('mode-btn');
 const modeSpan = document.getElementById('mode');
-
+const regionsBtn = document.getElementById('regions-click');
+const regionsDropdown = document.getElementById('regions-dropdown');
+const regions = document.querySelectorAll('.region');
+const span = document.querySelector('.dropdown-placeholder');
 
 async function getAllCountries() {
   let data = await fetch('https://restcountries.com/v2/all');
@@ -15,4 +18,14 @@ async function getAllCountries() {
 getAllCountries();
 
 
-// Who wrote this shit? 
+regionsBtn.addEventListener('click', () => {
+  regionsDropdown.classList.toggle('show');
+})
+
+regions.forEach((region) => {
+  region.addEventListener('click', () => {
+    span.textContent = region.textContent;
+    regionsDropdown.classList.remove('show');
+  })
+  // console.log(regions);
+})
