38585e3a66f86c148d3e75fae138234c2ffa124f
Ubejd <ubejd.haziri2005@gmail.com>
2021-10-30 15:20:37 +0200

    displayes all countries and styled the cards

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const regionsBtn = document.getElementById('regions-click');
 const regionsDropdown = document.getElementById('regions-dropdown');
 const regions = document.querySelectorAll('.region');
 const span = document.querySelector('.dropdown-placeholder');
+const dropdownIcon = document.querySelector('#regions i');
 
 async function getAllCountries() {
   let data = await fetch('https://restcountries.com/v2/all');
@@ -15,11 +16,33 @@ async function getAllCountries() {
   showAllCountries(res);
 }
 
-getAllCountries();
+function showAllCountries(data) {
+  data.forEach((country) => {
+    ctrSection.innerHTML += `
+    <div class="country-card">
+      <div class="flag"><img src="${country.flags.svg}"></div>
+      <div class="info">
+        <h4>${country.name}</h4>
+        <li class="info-item"><strong>Population</strong>:  ${country.population}</li>
+        <li class="info-item"><strong>Region</strong>:  ${country.region}</li>
+        <li class="info-item"><strong>Capital</strong>:  ${country.capital}</li>
+      </div>
+    </div>
+  `;
+  });
+}
 
+
+
+getAllCountries();
 
 regionsBtn.addEventListener('click', () => {
   regionsDropdown.classList.toggle('show');
+  if (dropdownIcon.className == 'fas fa-angle-down') {
+    dropdownIcon.className = 'fas fa-angle-up';
+  } else {
+    dropdownIcon.className = 'fas fa-angle-down';
+  }
 })
 
 regions.forEach((region) => {
@@ -27,5 +50,4 @@ regions.forEach((region) => {
     span.textContent = region.textContent;
     regionsDropdown.classList.remove('show');
   })
-  // console.log(regions);
 })
