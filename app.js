1355f0da8c72963cc5209e9593738f22bbba342a
Ubejd <ubejd.haziri2005@gmail.com>
2021-11-01 15:18:32 +0100

    implemented search functionallity

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const searchBar = document.getElementById('search-bar');
-const search = document.getElementById('search');
+const searchInput = document.getElementById('search-input');
 const ctrSection = document.getElementById('countries');
 const modeBtn = document.getElementById('mode-btn');
 const modeSpan = document.getElementById('mode');
@@ -8,6 +7,7 @@ const regionsDropdown = document.getElementById('regions-dropdown');
 const regions = document.querySelectorAll('.region');
 const span = document.querySelector('.dropdown-placeholder');
 const dropdownIcon = document.querySelector('#regions i');
+// const countries = document.querySelectorAll('.country-card');
 
 async function getAllCountries() {
   let data = await fetch('https://restcountries.com/v2/all');
@@ -23,13 +23,35 @@ function showAllCountries(data) {
       <div class="flag"><img src="${country.flag}"></div>
       <div class="info">
         <h4>${country.name}</h4>
-        <li class="info-item"><strong>Population</strong>:  ${country.population}</li>
+        <li class="info-item"><strong>Population</strong>:  ${country.population.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")}</li>
         <li class="info-item"><strong>Region</strong>:  ${country.region}</li>
-        <li class="info-item"><strong>Capital</strong>:  ${country.capital === 'undefined'? 'N/A': country.capital} </li>
+        <li class="info-item"><strong>Capital</strong>:  ${country.capital === 'undefined' ? 'N/A': country.capital} </li>
       </div>
     </div>
   `;
   });
+}
+
+async function getCountry(country) {
+  let data = await fetch(`https://restcountries.com/v2/name/${country}`);
+  let res = await data.json();
+  
+  showCountry(res);
+}
+
+// Show single country
+function showCountry(country) {
+  ctrSection.innerHTML = `
+  <div class="country-card">
+    <div class="flag"><img src="${country[0].flag}"></div>
+    <div class="info">
+      <h4>${country[0].name}</h4>
+      <li class="info-item"><strong>Population</strong>:  ${country[0].population.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")}</li>
+      <li class="info-item"><strong>Region</strong>:  ${country[0].region}</li>
+      <li class="info-item"><strong>Capital</strong>:  ${country[0].capital === 'undefined' ? 'N/A': country[0].capital} </li>
+    </div>
+  </div>
+`;
 }
 
 
@@ -49,5 +71,10 @@ regions.forEach((region) => {
   region.addEventListener('click', () => {
     span.textContent = region.textContent;
     regionsDropdown.classList.remove('show');
+    dropdownIcon.className = 'fas fa-angle-down';
   })
+})
+
+searchInput.addEventListener('change', (e) => {
+  getCountry(e.target.value);
 })
